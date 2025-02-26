a1ba3536c07cbe5289b91fe6e7ebdee51d2456be
Jasminex511 <jasmine1999511@gmail.com>
2025-02-26 16:36:12 -0500

    removed unused functions

--- consumers/base_consumer.py
+++ consumers/base_consumer.py
@@ -7,19 +7,3 @@ class BaseConsumer:
             .appName(appname) \
             .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0") \
             .getOrCreate()
-
-    def consume_message(self):
-        msg = self.consumer.poll(1.0)
-        if msg is None:
-            return None
-        if msg.error():
-            print(f"Consumer error: {msg.error()}")
-            return None
-        return msg.value()
-
-    def close(self):
-        if self.consumer:
-            print("Closing Kafka consumer...")
-            self.consumer.close()
-            self.consumer = None
-            print("Kafka consumer closed successfully.")
